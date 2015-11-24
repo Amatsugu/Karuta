1aa9d06c14844cdd5852f999f483a72f5a633b75
TheDarkVoid <Rohit.Khamraj@gmail.com>
2015-11-24 00:55:03 -0500

    Command: options implementation and help command

--- Karuta/Command.cs
+++ Karuta/Command.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 namespace com.LuminousVector.Karuta
 {
+	//Base command
 	public class Command
 	{
 		public string name { get { return _name; } }
 		public string helpMessage { get { return _helpMessage; } }
+		public string usageMessage { get { return _usageMessage; } }
 		private string _name;
 		private string _helpMessage;
+		private string _usageMessage;
 		protected Action _action;
 
 		public Command(string name, Action action)
@@ -26,6 +29,13 @@ namespace com.LuminousVector.Karuta
 			_helpMessage = helpMessage;
 		}
 
+		public Command(string name, string helpMessage, string usageMessage)
+		{
+			_name = name;
+			_helpMessage = helpMessage;
+			_usageMessage = usageMessage;
+		}
+
 		public Command(string name, string helpMessage, Action action)
 		{
 			_name = name;
@@ -33,32 +43,130 @@ namespace com.LuminousVector.Karuta
 			_action = action;
 		}
 
+		public Command(string name, string helpMessage, string usageMessage, Action action)
+		{
+			_name = name;
+			_helpMessage = helpMessage;
+			_usageMessage = usageMessage;
+			_action = action;
+		}
+
 		public virtual void Run(string[] args)
 		{
 			_action.Invoke();
 		}
+
+		public string GetOptions(string[] args)
+		{
+			string options = "";
+			foreach(string a in args)
+			{
+				if(a[0] == '-')
+				{
+					options += a.Remove(0, 1);
+				}
+			}
+			return options;
+		}
+
+		public int GetIndexOfOption(string[] args, char option)
+		{
+			for(int i = 0; i < args.Length; i++)
+			{
+				if (args[i] == "-" + option)
+				{
+					if (args.Length - 1 >= i + 1)
+						return i + 1;
+					else
+						return -1;
+				}
+			}
+			return -1;
+		}
 	}
 
+	//User control command
 	public class UserCommand : Command
 	{
-		public UserCommand() : base("user", "user -s [username]") { }
+		public UserCommand() : base("user", "Modify the current user.", "user -s [username]") { }
 
 		public override void Run(string[] args)
 		{
-			if (args.Length <= 1)
+			if (args.Length < 2)
 			{
-				Karuta.Say("A user must be specified");
-				Karuta.Say(helpMessage);
+				Karuta.SayQuietly("Not enough parameters");
+				Karuta.SayQuietly(usageMessage);
 			}
 			else
 			{
-				Karuta.SetUser(args[1]);
+				string opt = GetOptions(args);
+                if (opt.Length == 1)
+				{
+					if(opt == "s")
+					{
+						int index = GetIndexOfOption(args,'s');
+						if (index != -1)
+						{
+							Karuta.user = args[index];
+						}
+						else
+						{
+							Karuta.SayQuietly("A user must be specified");
+							Karuta.SayQuietly(usageMessage);
+						}
+					}else
+					{
+						Karuta.SayQuietly("Invalid option: '" + opt.Remove(opt.IndexOf('s'),1) + "'");
+						Karuta.SayQuietly(usageMessage);
+					}
+				}else
+				{
+					if (opt.Length != 0)
+					{
+						Karuta.SayQuietly("Extra options: '" + opt.Remove(opt.IndexOf('s'), 1) + "'");
+						Karuta.SayQuietly(usageMessage);
+					}else
+					{
+						Karuta.SayQuietly("Invalid command format.");
+						Karuta.SayQuietly(usageMessage);
+					}
+				}
 			}
 		}
 	}
 	
-	public class MakeCommand : Command
+	//Help command
+	public class HelpCommand : Command
 	{
-		public MakeCommand() : base("make", "Use help make for usage instructions") { }
+		public HelpCommand() : base("help", "show this screen") { }
+
+		public override void Run(string[] args)
+		{
+			if(args.Length == 1)
+			{
+				Karuta.SayQuietly("The available commands are:");
+				foreach(Command c in Karuta.commands.Values)
+				{
+					Karuta.SayQuietly("\t" + c.name + "\t" + c.helpMessage);
+					if (c.usageMessage != null)
+						Karuta.SayQuietly("\t\tUsage: " + c.usageMessage);
+				}
+			}else
+			{
+				for(int i = 1; i < args.Length; i++)
+				{
+					if(Karuta.commands.ContainsKey(args[i]))
+					{
+						Command c = Karuta.commands[args[i]];
+						Karuta.SayQuietly("\t" + c.name + "\t" + c.helpMessage);
+						if (c.usageMessage != null)
+							Karuta.SayQuietly("\t\tUsage: " + c.usageMessage);
+					}else
+					{
+						Karuta.SayQuietly("No such command '" + args[i] + "'");
+					}
+				}
+			}
+		}
 	}
 }
--- Karuta/Karuta.cs
+++ Karuta/Karuta.cs
@@ -10,13 +10,23 @@ namespace com.LuminousVector.Karuta
 {
 	public static class Karuta
 	{
+		public static Dictionary<string, Command> commands { get { return _commands; } }
+		public static string user
+		{
+			set
+			{
+				Say("Setting user to: " + value);
+				_user = value;
+			}
+		}
 		private static bool _isRunning = false;
 		private static string _input;
 		private static string _user;
 		private static SpeechSynthesizer _voice;
-		private static Dictionary<string,Command> commands = new Dictionary<string,Command>();
+		private static Dictionary<string,Command> _commands = new Dictionary<string,Command>();
 		static Karuta()
 		{
+			//Prepare Console
 			Console.Title = "Karuta";
 			Console.BackgroundColor = ConsoleColor.DarkMagenta;
 			Console.ForegroundColor = ConsoleColor.White;
@@ -24,13 +34,15 @@ namespace com.LuminousVector.Karuta
 			Stopwatch sw = new Stopwatch();
 			sw.Start();
 			SayQuietly("Preparing Karuta...");
+			//Prepare Voice
 			_voice = new SpeechSynthesizer();
 			_voice.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Teen);
 			_user = "user";
-			RegisterCommand(new Command("stop", Close));
-			RegisterCommand(new Command("clear", Console.Clear));
+			//Register Commands
+			RegisterCommand(new Command("stop", "Stop Karuta", Close));
+			RegisterCommand(new Command("clear", "Clear the screen.", Console.Clear));
 			RegisterCommand(new UserCommand());
-			RegisterCommand(new MakeCommand());
+			RegisterCommand(new HelpCommand());
 			sw.Stop();
 			long elapsedT = sw.ElapsedTicks / (Stopwatch.Frequency / (1000L));
 			SayQuietly("Karuta is ready. Finished in " + elapsedT + "ms");
@@ -59,12 +71,7 @@ namespace com.LuminousVector.Karuta
 			}
 		}
 
-		public static void SetUser(string user)
-		{
-			Say("Setting user to: " + user);
-			_user = user;
-		}
-
+		//Stop the program and dispose dispoables
 		public static void Close()
 		{
 			_voice.Dispose();
@@ -72,6 +79,7 @@ namespace com.LuminousVector.Karuta
 			_isRunning = false;
 		}
 
+		//Say message with voice and name label
 		public static void Say(string message)
 		{
 			_voice.SpeakAsyncCancelAll();
@@ -79,11 +87,13 @@ namespace com.LuminousVector.Karuta
 			Console.WriteLine("Karuta: " + message);
 		}
 
+		//Say a message without name label or voice
 		public static void SayQuietly(string message)
 		{
 			Console.WriteLine(message);
 		}
 
+		//Invoke a command
 		public static void InvokeCommand(string command, string[] args)
 		{
 			if(commands.ContainsKey(command))
